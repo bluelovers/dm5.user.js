af8ee990ff14f01294878bfdefbdecc24602636c
bluelovers <codelovers@users.sourceforge.net>
2026-01-11 04:59:32 +0800

    init: empty tpl

---
+++ dm5_v2.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-10
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.dm5.com/m1638181-p12/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dm5.com
+// @grant        none
+// ==/UserScript==
+
+(function ()
+{
+	'use strict';
+
+	// Your code here...
+})();
